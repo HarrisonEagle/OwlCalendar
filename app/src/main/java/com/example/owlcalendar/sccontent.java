a1c4c254569cd5b14550987db575c41c9beca443
HarrisonKawagoe <hyzitbios@hotmail.com>
2019-07-08 21:59:05 +0900

    Initial commit

---
+++ app/src/main/java/com/example/owlcalendar/sccontent.java
@@ -0,0 +1,40 @@
+package com.example.owlcalendar;
+
+public class sccontent {
+    public String content;
+    public int year;
+    public int day;
+    public int month;
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public int getYear() {
+        return year;
+    }
+}
